b728ba55a10b63a39d8a18f8df21bd7fd0d56eaf
rickxz <herick.victor.rodrigues@hotmail.com>
2024-06-18 19:54:29 -0300

    feat(criar lembretes): disable button while form is submitting

--- js/criar_lembretes.js
+++ js/criar_lembretes.js
@@ -1,34 +1,38 @@
 import { checkUserLoggedIn, baseURL } from "./utils.js";
 
 async function postLembretes(e){
-    e.preventDefault()
+  checkUserLoggedIn();
+  e.preventDefault()
 
-
-    let textoLembrete = document.querySelector("#lembrete").value
+  // desativar botão enquanto o formulário estiver sendo enviado
+  const buttonSubmit = document.querySelector("button")
+  buttonSubmit.disabled = true
+  
+  
+  let textoLembrete = document.querySelector("#lembrete").value
 
   try{
     let resposta = await fetch(`${baseURL}/lembrete`, {
       method : "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
-    },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      },
       body : JSON.stringify({
         texto : textoLembrete
       })
     })
-
-    console.log("texto");
+    
     window.location.href = "/pages/lembretes/lembretes.html"
   }
-
+  
   catch(erro){
-  console.error("Deu ruim", erro);
-}
+    console.error("Deu ruim", erro);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-    const formSubmit = document.querySelector("form")
-    formSubmit.addEventListener("submit",postLembretes)
-
+  const formSubmit = document.querySelector("form")
+  formSubmit.addEventListener("submit",postLembretes)
+  
 })
